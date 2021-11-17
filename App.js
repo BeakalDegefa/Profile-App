1a190d4cd4f1415e4519d9586269006403caee3f
BeakalDegefa <84756882+BeakalDegefa@users.noreply.github.com>
2021-11-16 19:05:57 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
 import HomeScreen from "./screens/HomeScreen";
 import DetailsScreen from "./screens/DetailsScreen";
-
 import { StatusBar } from "expo-status-bar";
 
 const Stack = createNativeStackNavigator();
